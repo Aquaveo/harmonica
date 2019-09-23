1422f461b6731c5da35d20d3cf10e485bb929337
aclark <aclark@aquaveo.com>
2019-09-23 16:14:03 -0600

    Remove debug statement.

--- build.py
+++ build.py
@@ -7,8 +7,6 @@ if __name__ == "__main__":
     aquapi_url = os.environ.get('AQUAPI_URL', None)
     release_python = os.environ.get('RELEASE_PYTHON', 'False')
 
-    print(f'release_python = {release_python}')
-
     if release_python == 'True':
         os.system(f'devpi use {aquapi_url}')
         os.system(f'devpi login {aquapi_username} --password {aquapi_password}')
